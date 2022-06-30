ef6170e0eb39e0e23082ff8a82ca7472f723f79f
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-06-30 15:03:23 -0700

    if elif else by Python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,6 @@ import random
 
 number = random.randint(-10000, 10000)
 
-
-
 if number >= 0:
 
     l_digit = number % 10
@@ -14,10 +12,7 @@ else:
 
     l_digit = number % -10
 
-
-
-print("Last digit of {} is {}".format(number, l_digit), end='')
-
+    print("Last digit of {} is {}".format(number, l_digit), end='')
 
 
 if l_digit > 5:
