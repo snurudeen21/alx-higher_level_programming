d056bd19d4dd77054269b22c018a5519d343296d
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-07-15 15:14:31 -0700

    Import and Modules by Python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
 # 0_add.py
-
-
-
-
 
 if __name__ == "__main__":
 
     """Print the sum of 1 and 2."""
 
     from add_0 import add
-
-
 
     a = 1
 
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-
 # 2-args.py
-
-
 
 if __name__ == "__main__":
 
     """Print the number of and list of arguments."""
 
     import sys
-
-
 
     count = len(sys.argv) - 1
 
