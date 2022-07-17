973c2e20bfefe34c85cc23a25b30a7df5a51ee13
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-07-17 07:11:38 -0700

    Import and Modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops ={'+': add, '-': sub, '*': mul, '/': div}
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
 
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
