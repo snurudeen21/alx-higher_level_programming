0352bf9fd216b46a7e1bd300a314cde4b70f4ffd
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-07-23 05:51:07 -0700

    More Data Structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,14 +2,8 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if not a_dictionary:
 
         return (None)
 
-    else:
-
-        v = list(a_dictionary.values())
-
-        k = list(a_dictionary.keys())
-
-        return (k[v.index(max(v))])
+    return (max(a_dictionary, key=a_dictionary.get))
