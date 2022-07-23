ef99b201d683997418c6c019b8a7c91390ed68f7
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-07-23 04:19:21 -0700

    More Data Structure

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    Newsquare = []
+
+    i = 0
+
+    for x in matrix:
+
+        Newsquare.append([])
+
+        for y in matrix[i]:
+
+            Newsquare[i].append(y**2)
+
+        i += 1
+
+    return (Newsquare)
