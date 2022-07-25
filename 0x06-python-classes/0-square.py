e325b1a7c57cebe9f646cb937fc9e7e651fceb87
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-07-25 07:33:12 -0700

    Object Oreinted Programming

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines an empty square class"""
+
+
+class Square:
+    """Class Square that defines a square.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Object Oriented Progranning: Class, Object, Method, Attributes and Instances 
