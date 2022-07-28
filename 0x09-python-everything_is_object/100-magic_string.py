4caa9e3a7b1fa24988e5085788007207f4979b49
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-07-28 01:52:31 +0100

    Magic__String

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(i=[0]):
-    i[0] += 1
-    return str("BestSchool, " * (i[0] - 1)) + "BestSchool")
+def magic_string():
+    setattr(magic_string, "times", getattr(magic_string, "times", -1) + 1)
+    return "BestSchool" + ", BestSchool" * getattr(magic_string, "times", 0)
