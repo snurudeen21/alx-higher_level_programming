82add53dc9bf7bacceedca8afec4c08f95aab2f4
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-02 23:57:54 +0100

    Open__Append__file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""append file"""
+
+
+    def append_write(filename="", text=""):
+        """append file"""
+        with open(filename, mode="a", encoding="utf-8") as fm:
+            fm.write(text)
+        return len(text)
