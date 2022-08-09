d04cd615b134818db063c8702fb83a573ba9cec3
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-09 05:57:28 -0700

    Almost a Circle

--- 0x0C-python-almost_a_circle/tests/test_task_0.py
+++ 0x0C-python-almost_a_circle/tests/test_task_0.py
@@ -2,13 +2,14 @@
 import unittest
 from Area import square_area
 
+
 class TestSquareArea(unittest.TestCase):
-	def test_area(self):
-	#Test area when area >= 0
-		self.assertAlmostEqual(square_area(1), 1)
-		self.assertAlmostEqual(square_area(4), 16)
-		self.assertAlmostEqual(square_area(0), 0)
-		
-	def test_value(self):
-	#Raise value error when necesary
-		self.assertRaises(ValueError, square_area, -2)
+    def test_area(self):
+        # Test area when area >= 0
+        self.assertAlmostEqual(square_area(1), 1)
+        self.assertAlmostEqual(square_area(4), 16)
+        self.assertAlmostEqual(square_area(0), 0)
+
+    def test_value(self):
+        # Raise value error when necesary
+        self.assertRaises(ValueError, square_area, -2)
