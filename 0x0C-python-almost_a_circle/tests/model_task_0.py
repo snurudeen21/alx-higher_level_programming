d718fd1a3bf4848053cdc2b55905eec240acdc80
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-10 06:11:46 -0700

    Test_Base

--- 0x0C-python-almost_a_circle/tests/model_task_0.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-""" Calculate area of Square """
-
-
-def square_area(length):
-    if length < 0:
-        raise ValueError("Please enter a positive integer")
-    return length**2
