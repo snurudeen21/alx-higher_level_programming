9c87e755444af92c5a7be0c9019299d4682ce994
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-11 16:28:44 -0700

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 This module implements `base` class of all other classes in this project.
 The goal of it is to manage id attribute in all your future classes
 and to avoid duplicating the same code (by extension, same bugs)
@@ -7,11 +7,11 @@ and to avoid duplicating the same code (by extension, same bugs)
 
 
 class Base:
-  __nb_objects = 0
-  
-  def __init__(self, id=None):
-    if id is not None:
-      self.id = id
-    else:
-      Base.__nb_objects += 1
-      self.id = Base.__nb_objects
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
