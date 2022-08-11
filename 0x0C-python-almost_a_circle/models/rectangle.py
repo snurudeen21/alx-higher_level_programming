d959b87f1df666358e59235e5c9f0e98cb8f4905
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-11 16:45:25 -0700

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,7 +99,7 @@ class Rectangle(Base):
         """prints # shape of the rectangle
         """
         print('\n'*self.y, end='')
-        for length in range(self.height):
+        for l in range(self.height):
             print(' '*self.x + '#'*self.width)
 
     def update(self, *args, **kwargs):
