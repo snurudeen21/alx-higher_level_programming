87fe87ad4ccebe2c43f0d61fd03a0c2a5b5cda66
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-09-11 10:41:45 +0100

    Intro to SQL

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
--- Insert a new row in 'first_table' from db 'hbtn_0c_0'
--- id == 89
--- name == Holberton School
--- db name will be passed as arg to mysql cmd
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- inserts a new row in table first_table
+-- in database hbtn_0c_0
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
