ac99588e4a8613c2a125a8c7a1d7773b9c767621
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-09-11 10:59:51 +0100

    More permission

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Query to create the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- granting privileges
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
