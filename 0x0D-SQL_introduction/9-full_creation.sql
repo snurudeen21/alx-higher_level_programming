59de80338a6874ead19d45383e53c5677ab9a62b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-08-20 15:31:15 +0100

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,9 +3,9 @@
 CREATE TABLE IF NOT EXISTS second_table(
 id INT,
 name VARCHAR(256),
-score INT)
+score INT);
 INSERT INTO second_table
-(id, name, score)VALUES
+(id, name, score) VALUES
 (1, “John”, 10),
 (2, “Alex”, 3),
 (3, “Bob”, 14),
