9a540aa5332bffb358364b1d6fde833cf8e12a8d
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-08-20 17:15:48 +0100

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,5 @@
--- Alter the database to use UTF-8 (utf8mb4) encoding and utf8mb4_unicode_ci collation
+-- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Alter the table first_table to use UTF-8 (utf8mb4) encoding and utf8mb4_unicode_ci collation
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Alter the name field in first_table to use UTF-8 (utf8mb4) encoding and utf8mb4_unicode_ci collation
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
