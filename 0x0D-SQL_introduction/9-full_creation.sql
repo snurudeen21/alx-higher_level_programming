37aeef32301ab5791c21cdc8602ab5e13e90692c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-08-20 15:09:49 +0100

    sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- create a Table, add features and fields
+CREATE TABLE IF NOT EXISTS second_table (
+id INT,
+name VARCHAR(256),
+score INT)
+INSERT INTO second_table
+(id, name, score)values
+(d = 1, name = “John”, score = 10)
+(id = 2, name = “Alex”, score = 3)
+(id = 3, name = “Bob”, score = 14)
+(id = 4, name = “George”, score = 8);
