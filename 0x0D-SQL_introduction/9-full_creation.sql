c96ed7208edc2341f1a784c84eb5bf00ce1ed690
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-08-20 15:25:22 +0100

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,8 +5,8 @@ id INT,
 name VARCHAR(256),
 score INT);
 INSERT INTO second_table
-  (id, name, score)values
-  (d = 1, name = “John”, score = 10),
+  (id, name, score)VALUES
+  (id = 1, name = “John”, score = 10),
   (id = 2, name = “Alex”, score = 3),
   (id = 3, name = “Bob”, score = 14),
   (id = 4, name = “George”, score = 8);
