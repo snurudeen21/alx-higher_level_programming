3db043614b4568064a6972c6e747c26c4dabba72
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-08-21 17:18:54 +0100

    sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create new user if it doesn't exists
 -- grant all privileges to User
-CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
