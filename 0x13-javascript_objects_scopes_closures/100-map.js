7c1dce1da302d8be77ed91005810df715d86e33d
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-09-29 08:01:33 +0100

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (x, index) {
+const Arr = require('./100-data').list;
+const newArr = list.map(function (x, index) {
   return x * index;
 });
 
-console.log(list);
-console.log(newList);
+console.log(Arr);
+console.log(newArr);
