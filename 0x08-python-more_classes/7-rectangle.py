802a495cfb7f2797cfd4d7f3cd2786683a6f4a6c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-01-10 12:18:50 +0100

    More Classes

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,7 +68,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append(str(Rectangle.print_symbol)) for j in range(self.__width)]
+            [rect.append(Rectangle.print_symbol) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
