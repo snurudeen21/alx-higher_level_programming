a901e97fa352731e7478f33d28c3fe3f79f12837
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-01-10 12:38:18 +0100

    More Classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -63,7 +63,7 @@ def Queen(m_queen, nqueen):
 
     m_queen[nqueen] = -1
 
-    while((m_queen[nqueen] < len(m_queen) - 1)):
+    while ((m_queen[nqueen] < len(m_queen) - 1)):
 
         m_queen[nqueen] += 1
 
