7ba3fd68d1c7bfe51f17e28f09be00048627aa6b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-01-11 17:48:14 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,7 +5,6 @@
 def pascal_triangle(n=4500):
     """print pascal"""
     pascal = [[0]*i for i in range(1, n+1)]
-    cmpt = 0
     for i in range(n):
         pascal[i][0] = 1
         pascal[i][-1] = 1
