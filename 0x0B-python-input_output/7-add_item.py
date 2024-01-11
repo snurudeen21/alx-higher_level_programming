0ccc9b4c3daf07349fdf23f1d48fa08843ebc535
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-01-11 16:23:41 +0100

    I/O

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,5 +14,6 @@ if os.path.isfile(filename):
     obj = load_from_json_file(filename)
 else:
     obj = []
-obj.extend(sys.argv[1:])
+for i in range(len(sys.argv)):
+	obj.append(sys.argv[i])
 save_to_json_file(obj, filename)
