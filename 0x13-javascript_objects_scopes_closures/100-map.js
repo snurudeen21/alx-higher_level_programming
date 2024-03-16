7c5bc15e1451d4a4a539efd99c041e6dc6a10322
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-03-16 08:32:59 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* using map to create a new Array */
-const Arr = require('./100-data').list;
+const { Arr } = require('./100-data').list;
 
 const newArr = Arr.map((x, index) => x * index);
 console.log(Arr);
