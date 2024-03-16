e6c5e92dd9219fe6e5cd0c271010e648f66c86b9
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-03-16 05:49:06 +0100

    Javascript Warmup

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const numOccurrences = parseInt(process.argv[2]);
+
+if (isNaN(numOccurrences)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
+}
