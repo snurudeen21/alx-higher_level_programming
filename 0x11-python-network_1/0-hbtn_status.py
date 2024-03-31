6d70669cfa3a25422130f4d37399eeeb1158e041
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-01 00:24:07 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(req) as my_response:
         html = my_response.read()
-        print('Body my_response:')
+        print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf-8')))
